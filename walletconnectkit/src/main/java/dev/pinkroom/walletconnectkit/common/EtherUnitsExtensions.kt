63ba3636413643f639c1525877c28717ac37f5a5
Bruno Correia <bffcorreia@gmail.com>
2022-01-25 12:04:15 +0000

    Add personalSign method.

--- walletconnectkit/src/main/java/dev/pinkroom/walletconnectkit/common/EtherUnitsExtensions.kt
+++ walletconnectkit/src/main/java/dev/pinkroom/walletconnectkit/common/EtherUnitsExtensions.kt
@@ -7,3 +7,5 @@ import java.math.BigDecimal
 fun String.toWei(unit: Convert.Unit = Convert.Unit.ETHER): BigDecimal = Convert.toWei(this, unit)
 
 fun BigDecimal.toHex(): String = Numeric.toHexStringWithPrefixSafe(this.toBigInteger())
+
+fun String.toHex(): String = Numeric.toHexString(this.toByteArray())
